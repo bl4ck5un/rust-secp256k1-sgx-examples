a6879ecad407b609f492fda05015262515e80507
Fan Zhang <bl4ck5unxx@gmail.com>
2018-08-30 18:33:07 -0400

    add test vectors

--- app/src/main.rs
+++ app/src/main.rs
@@ -40,17 +40,16 @@ static ENCLAVE_FILE: &'static str = "enclave.signed.so";
 static ENCLAVE_TOKEN: &'static str = "enclave.token";
 
 extern {
-    fn sign(eid: sgx_enclave_id_t, retval: *mut sgx_status_t,
-                     some_string: *const u8, len: usize) -> sgx_status_t;
+    fn run_tests(eid: sgx_enclave_id_t, retval: *mut sgx_status_t) -> sgx_status_t;
 }
 
 fn init_enclave() -> SgxResult<SgxEnclave> {
-    
+
     let mut launch_token: sgx_launch_token_t = [0; 1024];
     let mut launch_token_updated: i32 = 0;
-    // Step 1: try to retrieve the launch token saved by last transaction 
+    // Step 1: try to retrieve the launch token saved by last transaction
     //         if there is no token, then create a new one.
-    // 
+    //
     // try to get the token saved in $HOME */
     let mut home_dir = path::PathBuf::new();
     let use_token = match dirs::home_dir() {
@@ -84,18 +83,18 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
     }
 
     // Step 2: call sgx_create_enclave to initialize an enclave instance
-    // Debug Support: set 2nd parameter to 1 
+    // Debug Support: set 2nd parameter to 1
     let debug = 1;
     let mut misc_attr = sgx_misc_attribute_t {secs_attr: sgx_attributes_t { flags:0, xfrm:0}, misc_select:0};
-    let enclave = try!(SgxEnclave::create(ENCLAVE_FILE, 
-                                          debug, 
+    let enclave = try!(SgxEnclave::create(ENCLAVE_FILE,
+                                          debug,
                                           &mut launch_token,
                                           &mut launch_token_updated,
                                           &mut misc_attr));
-    
-    // Step 3: save the launch token if it is updated 
+
+    // Step 3: save the launch token if it is updated
     if use_token == true && launch_token_updated != 0 {
-        // reopen the file with write capablity 
+        // reopen the file with write capablity
         match fs::File::create(&token_file) {
             Ok(mut f) => {
                 match f.write_all(&launch_token) {
@@ -112,8 +111,7 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
     Ok(enclave)
 }
 
-fn main() { 
-
+fn main() {
     let enclave = match init_enclave() {
         Ok(r) => {
             println!("[+] Init Enclave Successful {}!", r.geteid());
@@ -126,14 +124,11 @@ fn main() {
     };
 
     let input_string = String::from("This is a normal world string passed into Enclave!\n");
-    
-    let mut retval = sgx_status_t::SGX_SUCCESS; 
+
+    let mut retval = sgx_status_t::SGX_SUCCESS;
 
     let result = unsafe {
-        sign(enclave.geteid(),
-                      &mut retval,
-                      input_string.as_ptr() as * const u8,
-                      input_string.len())
+        run_tests(enclave.geteid(), &mut retval)
     };
 
     match result {
